9449f5c00fa918206d99a7399395fa28f6b92399
Selena Tuyen <sltuyen@ucdavis.edu>
2017-01-04 21:16:07 -0800

    Bar chart

--- bar.js
+++ bar.js
@@ -4,9 +4,9 @@ var x = d3.scale.linear()
     .domain([0, d3.max(data)])
     .range([0, 420]);
 
-d3.select(".chart")
-  .selectAll("div")
-    .data(data)
-  .enter().append("div")
-    .style("width", function(d) { return x(d) + "px"; })
-    .text(function(d) { return d; });
+d3.select(".chart")		//select chart container using class selector, TOA var chart = d3.select(".chart");
+  .selectAll("div")		//initiate data join by defining selection to which we join data, TOA var bar = chart.selectAll("div");
+    .data(data)			//join data to selection using seleciton.data, TOA var barUpdate = bar.data(data);
+  .enter().append("div")	//instantiate missing elements by appending to enter select, TOA var barEnter = barUpdate.enter().append("div");
+    .style("width", function(d) { return x(d) + "px"; })	//Now set width of each new bar as multiple of associated data value, d.
+    .text(function(d) { return d; });	//set text of each bar
